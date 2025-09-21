affd5ce9391b8907c1668fc477423459d417d22a
Felipe <felipecardenas2103@gmail.com>
2025-09-21 15:45:12 -0500

    Base para la modificación de productos

---
+++ src/main/java/edu/analisis/modificacion_de_productos_d1/Modificacion_de_Productos_D1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package edu.analisis.modificacion_de_productos_d1;
+
+/**
+ *
+ * @author felip
+ */
+public class Modificacion_de_Productos_D1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
