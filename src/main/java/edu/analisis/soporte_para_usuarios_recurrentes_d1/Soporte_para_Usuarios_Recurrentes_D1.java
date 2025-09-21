bf0892540fc3942447417429a248ffa5c6610d84
Felipe <felipecardenas2103@gmail.com>
2025-09-21 15:32:52 -0500

    Base para el soporte de usuarios recurrentes

---
+++ src/main/java/edu/analisis/soporte_para_usuarios_recurrentes_d1/Soporte_para_Usuarios_Recurrentes_D1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package edu.analisis.soporte_para_usuarios_recurrentes_d1;
+
+/**
+ *
+ * @author felip
+ */
+public class Soporte_para_Usuarios_Recurrentes_D1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
